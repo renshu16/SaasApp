19a461de771bc9ea8955030014b4f9fcc7f66f07
renshu16 <toothbond@wjmac.local>
2015-10-28 09:35:50 +0800

    create project

---
+++ SaasApp/Classes/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SaasApp
+//
+//  Created by ToothBond on 15/10/27.
+//  Copyright © 2015年 ToothBond. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ SaasAppTests/SaasAppTests.m
@@ -0,0 +1,39 @@
+//
+//  SaasAppTests.m
+//  SaasAppTests
+//
+//  Created by ToothBond on 15/10/27.
+//  Copyright © 2015年 ToothBond. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface SaasAppTests : XCTestCase
+
+@end
+
+@implementation SaasAppTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
